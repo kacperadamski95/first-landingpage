02f655c76530f9bb0fcb252389e470f82b4c0b45
Kacper Adamski <kacper.adamski95@gmail.com>
2022-03-04 12:57:52 +0100

    adjusted menu's icons in js

---
+++ js/menu.js
@@ -0,0 +1,43 @@
+window.addEventListener('DOMContentLoaded', (event) => {   
+    //cały kod wykonujemy w DOMContentLoaded i dodajemy EventListenera na DOMContentLoaded tylko raz
+    const menuIconHamburger = document.querySelector(".menu-icon");
+
+    menuIconHamburger.addEventListener("click", (event) => {
+    
+        const navigationMobile = document.querySelector(".navigation--mobile");
+        
+        navigationMobile.classList.add("navigation--mobile--active");
+        
+    const menuIconClose = document.querySelector(".menu-icon-close");
+
+    menuIconClose.addEventListener("click", (event) => { 
+
+        navigationMobile.classList.remove("navigation--mobile--active");
+
+        menuIconHamburger.classList.add("menu--icon--hidden");
+
+        menuIconHamburger.classList.remove("menu--icon--hidden");
+
+    })
+
+
+
+        // menuIconHamburger.classList.add("menu--icon--hidden"); - TO JEST NUMER 2 to odkomentować jak wrzucę przywracanie hamburherowej ikonki
+    
+    /* 2. schowaj ikonkę hamburgera:
+    dodaj klasę (.menu--icon--hidden)
+    */
+    });
+    
+    /* 1. złap ikonkę X (zamknięcie menu) - const menuIconClose(zmienić klasę w html) = document.querySelector(".x-icon");
+    dodaj do niej EventListenera takiego jak do ikonki hamburgera - menuIconHamburger.addEventListener("click touch", (event) => {
+        const navigationMobile = document.querySelector(".navigation--mobile");
+    i z elementu z klasą navigation--mobile usuń klasę navigation--mobile--active (do wygooglowania) (zamiast add będzie remove raczej, ale sprawdzić)   navigationMobile.classList.remove?("navigation--mobile--active");
+    3. musze później przywrócić hamburgerową ikonkę - usunięcie klasy (.menu--icon--hidden)  menuIconHamburger.classList.remove?("menu--icon--hidden");
+    */
+
+    
+});
+
+
+
