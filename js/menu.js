6b17d056ee4ca8b603f9ba41ebf09781ff7613e8
Kacper Adamski <kacper.adamski95@gmail.com>
2022-03-08 21:32:17 +0100

    added a few things in js to practise

--- js/menu.js
+++ js/menu.js
@@ -24,11 +24,30 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
     //adding new background on body when scrolling
     const bodyBody = document.querySelector("body");
+    const introductionText = document.querySelector(".introduction__text");
 
     window.addEventListener("scroll", (event) => {
 
         bodyBody.classList.add("body--scroll");
     });
+
+    const bodyBackgroundDesktop = document.querySelector(".body--background--desktop");
+
+    window.addEventListener("resize", (event) => {
+        //Na dole mam ten sam warunek, ale zapisany w wersji skróconej, używać tamtego
+        // if (window.innerWidth > 769) {
+        //     bodyBody.classList.add("body--background--desktop");
+        // }
+
+        window.innerWidth > 769 ? bodyBody.classList.add("body--background--desktop") : null;
+    });
+
+    const siemanko = document.querySelector(".section--introduction h1");
+
+        siemanko.addEventListener("click", (event) => {
+            //chcę otworzyć link do contact
+            window.location.href = "file:///C:/Users/Kacpa/Desktop/programowanie/first-landingpage/contact.html";
+        });
 });
 
 
