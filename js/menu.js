80e100d463575650197b37933c39dd374fc4469c
Kacper Adamski <kacper.adamski95@gmail.com>
2022-03-10 15:35:53 +0100

    added some functions in js

--- js/menu.js
+++ js/menu.js
@@ -6,9 +6,9 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const navigationMobile = document.querySelector(".navigation--mobile");
     //dodałem event listenera na kliknięcie ikonki hamburgera
     menuIconHamburger.addEventListener("click", (event) => {
-    //tutaj na kliknięciu pokazuje się nawigacja
+        //tutaj na kliknięciu pokazuje się nawigacja
         navigationMobile.classList.add("navigation--mobile--active");
-    //tutaj na kliknięciu chowam ikonkę hamburgera
+        //tutaj na kliknięciu chowam ikonkę hamburgera
         menuIconHamburger.classList.add("menu--icon--hidden");
     });
     //na kliknięcie ikonki X 
@@ -17,14 +17,14 @@ window.addEventListener('DOMContentLoaded', (event) => {
         navigationMobile.classList.remove("navigation--mobile--active");
         //usuwam klasę, która sprawia, że jest niewidoczna ikonka hamburgera
         menuIconHamburger.classList.remove("menu--icon--hidden");
-        
 
-        
+
+
     });
 
     //adding new background on body when scrolling
     const bodyBody = document.querySelector("body");
-    const introductionText = document.querySelector(".introduction__text");
+    // const introductionText = document.querySelector(".introduction__text"); - czy to jest potrzebne
 
     window.addEventListener("scroll", (event) => {
 
@@ -44,10 +44,42 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
     const siemanko = document.querySelector(".section--introduction h1");
 
-        siemanko.addEventListener("click", (event) => {
-            //chcę otworzyć link do contact
-            window.location.href = "file:///C:/Users/Kacpa/Desktop/programowanie/first-landingpage/contact.html";
-        });
+    siemanko.addEventListener("click", (event) => {
+        //chcę otworzyć link do contact
+        window.location.href = "file:///C:/Users/Kacpa/Desktop/programowanie/first-landingpage/contact.html";
+    });
+
+    //paragaraf w index i dwie klasy w css
+    //przy kliknięciu i przytrzymaniu myszki na "dole" na h4 (email) ma pokazać się .email-paragraph , czemu zmienia się czcionka, ale nie wyświetla się tekst, który dodałem w paragrafie,jak jest z połączeniem klasy
+    //która nie jest w htmlu, tylko w css, ale nie jest przypisana do tego paragrafu, do paragrafu przypisana jest klasa z display none, której i tak tu nie używam
+    const emailTextInformation = document.querySelector(".footer__email h4");
+    const textMouseUp = document.querySelector(".email--paragraph")
+    emailTextInformation.addEventListener("mousedown" , (event) => {
+        if ("mousedown") { 
+            emailTextInformation.classList.add("email-paragraph-information");
+        }
+        // else { 
+        //     emailTextInformation.classList.add(textMouseUp);
+        // }
+    });
+
+    //dblclick - chcę żeby na doubleclick na tekscie nad ikonką instagrama przekierowało mnie na https://www.instagram.com/kacper.adamskii/
+    const instagramLink = document.querySelector(".footer_instagram");
+    instagramLink.addEventListener("dblclick", (event) => {
+        window.location.href = "https://www.instagram.com/kacper.adamskii/";
+    });
+    //chcę, żeby poniżej rozdzielczości 600px kolor tła zmieniał się na pomarańczowy - czemu jak wezmę z powrotem to nie pokazuje się biały tylko pomarańczowy
+    const bodyBackgroundMobile = document.querySelector(".body--background--mobile")
+    window.addEventListener("resize", (event) => {
+        if (window.innerWidth < 600) {
+            bodyBody.classList.add("body--background--mobile");
+        }
+    });
+    window.addEventListener("resize", (event) => {
+        if (window.innerWidth < 768) {
+            bodyBody.classList.add("body--background--kurdebele");
+        }
+    });
 });
 
 
